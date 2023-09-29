8f184da202335f260d8c2f7dc0579b062561ade2
manuelDanaa <manueldanaabegail@gmail.com>
2023-09-29 15:00:35 +0800

    Third Commit

---
+++ app/src/main/java/com/example/brightreads/HomeActivity.java
@@ -0,0 +1,51 @@
+package com.example.brightreads;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        SharedPreferences sharedpreferences = getSharedPreferences("shared_prefs", Context.MODE_PRIVATE);
+        String username = sharedpreferences.getString("username", "").toString();
+        Toast.makeText(getApplicationContext(), "Welcome "+username,Toast.LENGTH_SHORT).show();
+
+        CardView exit = findViewById(R.id.cardExit);
+        exit.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                SharedPreferences.Editor editor = sharedpreferences.edit();
+                editor.clear();
+                editor.apply();
+                startActivity(new Intent(HomeActivity.this,LoginActivity.class));
+            }
+        });
+
+        CardView MyLibrary = findViewById(R.id.cardMyLibrary);
+        MyLibrary.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(HomeActivity.this,MyLibraryActivity.class));
+            }
+        });
+
+        CardView books = findViewById(R.id.cardBooks);
+        books.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(HomeActivity.this,BooksActivity.class));
+            }
+        });
+    }
+}
--- app/src/main/java/com/example/brightreads/LoginActivity.java
+++ app/src/main/java/com/example/brightreads/LoginActivity.java
@@ -2,7 +2,9 @@ package com.example.brightreads;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -31,10 +33,21 @@ public class LoginActivity extends AppCompatActivity {
             public void onClick(View view) {
                 String username = edUsername.getText().toString();
                 String password = edPassword.getText().toString();
+                Database db = new Database(getApplicationContext(), "BrightReads", null, 1);
                 if(username.length()==0 || password.length()==0){
                     Toast.makeText(getApplicationContext(),"Fill up the form", Toast.LENGTH_SHORT).show();
                 }else {
-                    Toast.makeText(getApplicationContext(), "Login Success", Toast.LENGTH_SHORT).show();
+                    if(db.login(username,password)==1) {
+                        Toast.makeText(getApplicationContext(), "Login Success", Toast.LENGTH_SHORT).show();
+                        startActivity(new Intent(LoginActivity.this, HomeActivity.class));
+                        SharedPreferences sharedPreferences = getSharedPreferences("shared_prefs", Context.MODE_PRIVATE);
+                        SharedPreferences.Editor editor = sharedPreferences.edit();
+                        editor.putString("username", username);
+                        editor.apply();
+                    }else{
+                        Toast.makeText(getApplicationContext(), "Invalid Input", Toast.LENGTH_SHORT).show();
+                    }
+
                 }
             }
         });
